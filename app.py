4a908d2e403b296edae77d64386b1ae462af0fd0
Elinvar-pro <a06337786@gmail.com>
2026-05-07 13:10:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -925,4 +925,4 @@ if __name__ == '__main__':
     print("Пароль кладовщика: 123")
     print("=" * 50)
     if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+        app.run(debug=False, host='0.0.0.0', port=5000)
